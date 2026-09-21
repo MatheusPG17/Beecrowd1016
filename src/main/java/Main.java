741570c8abcaef8cd319af49fb441d79b283aef4
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:21:18 -0300

    add: comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         int dist, minutos;
         
+        //entrada de dados
         dist = leia.nextInt();
         
+        //processamento
         minutos = dist*2;
         
+        //saida de dados
         System.out.println(minutos + " minutos");
     }
 }
